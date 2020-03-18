5ea0d0385a4ee1b03d87b052b96c0966adc47d3a
serra-vp <vincent.serra@boom.camp>
2020-03-18 10:26:14 +0800

    forgot to save

--- addessbook/Objects/Query.php
+++ addessbook/Objects/Query.php
@@ -16,8 +16,8 @@ Class Query extends Connection {
 		$fetch_address_statement = $this->connect()->prepare("SELECT * FROM address");
 		$fetch_address_statement->execute();
 		
-		return $fetch_address_statement -> fetch();
-		echo '<script>console.log($fetch_address_statement)</script>';
+		return $asd = $fetch_address_statement -> fetch();
+		echo '<script>console.log($asd)</script>';
 
 		// $get_test_query = $database -> prepare("SELECT * FROM analysis, test WHERE analysis.analysis_ID = test.test_analysis_ID AND test.test_analysis_ID = ?");
 		// $get_test_query -> execute(array($selected_analysis_ID));
